a226150fd00e144a600b2011c1c5a7c870743c75
Eduardo Cesar Silva de Ávila <unkeniy8432@gmail.com>
2022-05-25 18:55:07 -0300

    savedefinitivo

--- src/pages/frequencia/index.js
+++ src/pages/frequencia/index.js
@@ -1,10 +1,29 @@
-
-
+import axios from "axios"
+import{ useState } from "react"
 
 export default function Index() {
+    const [texto,setTexto] = useState('');
+    const [caracter,setCaracter] = useState('');
+    const [resposta,setResposta]= useState('');
+
+    async function verificarFrequência(){
+        const resp = await axios.get(`http://localhost:5000/dia2/freqcaracter/${texto}/${caracter}`);
+        setResposta(resp);
+    }
     return(
         <main>
             <h1>Frequência</h1>
+
+            <div>
+                Texto:<input type="text" value={texto} onChange={e => setTexto(e.target.value)}></input>
+            </div>
+            <div>
+                Caracter:<input type="text" value={caracter} onChange={e => setCaracter(e.target.value)}></input>
+            </div>
+            <div>
+                <button onClick ={verificarFrequência}>verificar</button>
+            </div>
+            <div>Frequência = {resposta}</div>
         </main>
     )
 }
