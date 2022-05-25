2ae742f5046760ace614d422d81bdd6f042c6039
Eduardo Cesar Silva de Ávila <unkeniy8432@gmail.com>
2022-05-25 17:57:00 -0300

    save 2

--- src/pages/corprimaria/index.js
+++ src/pages/corprimaria/index.js
@@ -1,10 +1,29 @@
-
-
+import axios from "axios"
+import{ useState} from "react"
 
 export default function Index() {
+    const [cor,setCor] = useState('');
+    const[resposta,setResposta]=useState('');
+
+    function verificarCorPrimaria(){
+        let resp= axios.get('http://localhost:5000/dia2/corprimaria/' + cor)
+        if(resp.data.primaria === true){
+            setResposta('SIM!');
+        }else{
+            setResposta('NÃO!');
+        }
+    }
     return(
         <main>
             <h1>Cor Primária</h1>
+
+            <div>
+                Cor:<input type="text" value={cor} onChange={e => setCor(e.target.value)}></input>
+            </div>
+            <div>
+                <button onClick ={verificarCorPrimaria}>verificar</button>
+            </div>
+            <div>É cor primária? = {resposta}</div>
         </main>
     )
 }
--- src/pages/ingresso/index.js
+++ src/pages/ingresso/index.js
@@ -1,10 +1,38 @@
-
-
+import axios from 'axios'
+import { useState } from 'react';
 
 export default function Index() {
+    const [qtdInteira,setQtdInteiras]= useState(0);
+    const [qtdMeias,setQtdMeias]= useState(0);
+    const [diaSemana,setDiaSemana]= useState('');
+    const [nacionalidade,setNacionalidade]= useState('');
+    const [total,setTotal]= useState(0);
+
+    async function Calcular(){
+    const resp = await axios.post('http://localhost:5000/dia2/ingressocinema', {
+        qtd1:qtdInteira,
+        qtd2:qtdMeias,
+        dia:diaSemana,
+        tipo:nacionalidade 
+    }); 
+        setTotal(resp.data.total);
+    }
+
     return(
         <main>
             <h1>Ingresso</h1>
+            <div>Quantidade Inteiras: <input type='text' value={qtdInteira}onChange={e => setQtdInteiras(Number(e.target.value))} />
+            </div>
+            <div>Quantidade Meias: <input type='text'value={qtdMeias}onChange={e => setQtdMeias(Number(e.target.value))} /></div>
+            <div>Dia da semana: <input type='text' value={diaSemana} onChange={e =>setDiaSemana(e.target.value)} /></div>
+            <div>Nacionalidade: <input type='text' value={nacionalidade} onChange={e =>setNacionalidade(e.target.value)}/></div>
+            <div>
+                <button onClick={Calcular}>Calcular</button>
+            </div>
+            <div>
+                O total é R$: {total}
+            </div>
         </main>
+            
     )
 }
